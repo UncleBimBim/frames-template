49453233b6f15214a5ba0e0961e3d6e0afa9f257
UncleBimBim <kymlamsam@gmail.com>
2024-05-25 23:33:11 +1000

    removed 'stage' button text and replaced with 'input' text and emojis

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -6,18 +6,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
-
-  const text = message.input || '';
-  let state = {
-    page: 0,
-  };
-  try {
-    state = JSON.parse(decodeURIComponent(message.state?.serialized));
-  } catch (e) {
-    console.error(e);
+  if (message?.input) {
+    text = message.input;
   }
 
   /**
@@ -34,7 +24,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `State: ${state?.page || 0}`,
+          label: `${text} 😂🥕😊🚨⬇️`,
         },
         {
           action: 'link',
